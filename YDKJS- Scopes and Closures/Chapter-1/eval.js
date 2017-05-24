67179a3731d82ad29172ee5307be93ccc22c3925
shabbir1993 <syed_shabbir_haider@hotmail.com>
2017-05-24 19:01:20 +0500

    More examples , with and eval

---
+++ YDKJS- Scopes and Closures/Chapter-1/eval.js
@@ -0,0 +1,8 @@
+function foo(str, a) {
+	eval( str ); // cheating!
+	console.log( a, b );
+}
+
+var b = 2;
+
+foo( "var b = 3;", 1 ); // 1 3
---
+++ YDKJS- Scopes and Closures/Chapter-1/with.js
@@ -0,0 +1,17 @@
+var obj = {
+	a: 1,
+	b: 2,
+	c: 3
+};
+
+// more "tedious" to repeat "obj"
+obj.a = 2;
+obj.b = 3;
+obj.c = 4;
+
+// "easier" short-hand
+with (obj) {
+	a = 3;
+	b = 4;
+	c = 5;
+}
